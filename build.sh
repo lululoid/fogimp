cc4479057882412fbd8cf0683bebf29c83f9c4e2
lululoid <ereiwn@gmail.com>
2024-07-25 07:38:57 +0700

    Add: automatically install module

--- build.sh
+++ build.sh
@@ -34,9 +34,12 @@ sed -i "s/\(^version=v\)[0-9.]*\(.*\)/\1$version\2/; s/\(^versionCode=\)[0-9]*/\
 module_name=$(sed -n 's/^id=\(.*\)/\1/p' module.prop)
 
 # Create a zip package
-7za a "packages/$module_name-v${version}_$versionCode-beta.zip" \
+package_name="packages/$module_name-v${version}_$versionCode-beta.zip"
+7za a "$package_name" \
 	META-INF \
 	boot_config.sh \
 	customize.sh \
 	module.prop \
 	service.sh
+
+su -c "magisk --install-module $package_name" || echo "  > ROOT needed to install this module"
